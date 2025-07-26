074225966b33a2d562393903ce1f2d318771bb06
Mohammed Raziq V <anuvraziq@gmail.com>
2025-07-27 02:30:00 +0530

    completed the first level of set up especially backend

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from 'dotenv';
+import connectDB from './lib/db.js';
+
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+if (!process.env.MONGODB_URI) {
+  console.error("❌ MONGODB_URI is not defined in .env");
+  process.exit(1);
+}
+
+
+app.use("/api/auth",authRoutes)
+
+connectDB().then(()=>{ 
+  console.log("The database connected successfully")
+  app.listen(PORT, ()=>{
+    console.log(`server is running on PORT no: ${PORT}`);
+})
+}).catch((err)=>{
+  console.error("Database connection failed"+err.message);
+  process.exit(1)
+})
+
+
+
+
