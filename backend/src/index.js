81069edb0eddee609afd4e596e87ce5aebd69495
Mohammed Raziq V <anuvraziq@gmail.com>
2025-08-05 01:16:51 +0530

    completed everything related to user and started working on messaging

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from 'dotenv';
 import connectDB from './lib/db.js';
+import cookieParser from "cookie-parser"
 
 
 dotenv.config()
@@ -14,7 +15,8 @@ if (!process.env.MONGODB_URI) {
 }
 
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 
 connectDB().then(()=>{ 
